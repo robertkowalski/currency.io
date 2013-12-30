ea123f7b341dc08c0917f5771e327a201bb2725b
Robert Kowalski <rok@kowalski.gd>
2013-12-30 01:51:46 +0100

    add MMK, remove other ones

--- public/javascripts/dataset.js
+++ public/javascripts/dataset.js
@@ -5,20 +5,12 @@ String.prototype.count = function(str) {
 var currencies = JSON.stringify({
   USD: { name: "US Dollar", symbol: "$", rate_usd: 1 },
   EUR: { name: "Euro", symbol: "€", rate_usd: 1 },
-  GBP: { name: "British Pound", symbol: "£", rate_usd: 1 },
-  CAD: { name: "Canadian Dollar", symbol: "$", rate_usd: 1 },
-  AUD: { name: "Australian Dollar", symbol: "$", rate_usd: 1 },
-  JPY: { name: "Japanese Yen", symbol: "¥", rate_usd: 1 },
-  KRW: { name: "South Korean Won", symbol: "₩", rate_usd: 1 },
-  NZD: { name: "New Zealand Dollar", symbol: "$", rate_usd: 1 },
-  CHF: { name: "Swiss Franc", symbol: "chf", rate_usd: 1 },
-  SGD: { name: "Singapore Dollar", symbol: "$", rate_usd: 1 },
-  HKD: { name: "Hong Kong Dollar", symbol: "$", rate_usd: 1 }
+  MMK: { name: "Myanmar Kyan", symbol: "M", rate_usd: 1 }
 });
 
 if (!localStorage.currencies || localStorage.currencies.count(':') !== currencies.count(':')) {
   localStorage.currencies = currencies;
-  localStorage.from_to = '{ "from": "JPY", "to": "AUD" }';
+  localStorage.from_to = '{ "from": "EUR", "to": "MMK" }';
 }
 
 window.currencies = JSON.parse(localStorage.currencies);
